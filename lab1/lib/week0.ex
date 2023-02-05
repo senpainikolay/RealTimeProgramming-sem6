74316de51a3d9ed6d650f4a6d15f7b9ab007ee04
senpainikolay <nicolae32gherman@gmail.com>
2023-02-05 13:22:13 +0200

    Lab1 Week0 Init

---
+++ lab1/lib/week0.ex
@@ -0,0 +1,8 @@
+
+defmodule Week0 do
+  def hello() do
+    "Hello PTR"
+  end
+end
+
+IO.puts Week0.hello()
---
+++ lab1/test/week0_test.exs
@@ -0,0 +1,8 @@
+defmodule Week0Test do
+  use ExUnit.Case
+  doctest Week0
+
+  test "greets the world" do
+    assert Week0.hello() == "Hello PTR"
+  end
+end
