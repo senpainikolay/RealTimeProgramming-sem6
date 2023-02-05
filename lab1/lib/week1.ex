ab555cd5cde60858014db81913d9a6c96c9233f9
senpainikolay <nicolae32gherman@gmail.com>
2023-02-05 18:55:23 +0200

    add ex2,3

--- lab1/lib/week1.ex
+++ lab1/lib/week1.ex
@@ -15,6 +15,22 @@ defmodule Week1 do
     rem(x,y) == 0
   end
 
+  #EX2
+  def cylinderArea(h,r) do
+    2 * 3.1415 * r * h + 2 * 3.1415 * r * r
+  end
+
+  #EX3
+  def reverseList(a)when is_number(a),do: a
+  def reverseList([]),do: []
+  def reverseList([h|t]) do
+    reverseList(t) ++ [h]
+  end
+
+
+
 end
 
 IO.puts(Week1.isPrime(7))
+IO.puts(Week1.cylinderArea(3,4))
+IO.inspect(Week1.reverseList([1,2,3]))
